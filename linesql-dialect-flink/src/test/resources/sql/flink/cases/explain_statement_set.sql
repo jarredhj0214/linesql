7c96cfebc0c27f622c85aaabf50270820c482e4f
jarredhj0214 <jarred0214@gmail.com>
2026-09-10 12:50:36 +0800

    Add Flink explain statement set coverage

---
+++ linesql-dialect-flink/src/test/resources/sql/flink/cases/explain_statement_set.sql
@@ -0,0 +1,8 @@
+EXPLAIN PLAN FOR
+STATEMENT SET
+BEGIN
+INSERT INTO ads.user_summary (user_id, user_name)
+SELECT id, name FROM ods.users;
+INSERT OVERWRITE ads.order_summary (order_id, amount)
+SELECT id, amount FROM ods.orders;
+END
